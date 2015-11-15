78daf9e61c81025d6b8e804cbf55ebb92f90012a
84zume <hideki.hashizume@live.jp>
2015-11-15 10:50:25 +0900

    練習

---
+++ Chapter3/ch3-interface-basis/TheInterface/TheInterface/ISimpleInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheInterface
+{
+    public interface ISimpleInterface
+    {
+        void ThisMethodRequiresImplementations();
+
+        string ThisStringPropertyNeedsImplementingToo
+        {
+            get;
+            set;
+        }
+
+        int ThisIntPropertyOnlyNeedsAsGetter
+        {
+            get;
+        }
+
+        event EventHandler<EventArgs> InterfacesCanContainsEventToo;
+    }
+}
---
+++ Chapter3/ch3-interface-basis/TheInterface/TheInterface/SimpleInterfaceImplementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TheInterface
+{
+    public class SimpleInterfaceImplementation : ISimpleInterface
+    {
+
+        private int _encapsulatedInt;
+
+        public int ThisIntPropertyOnlyNeedsAsGetter
+        {
+            get { return _encapsulatedInt; }
+            set { _encapsulatedInt = value; }
+        }
+
+        public string ThisStringPropertyNeedsImplementingToo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler<EventArgs> InterfacesCanContainsEventToo;
+
+        public void ThisMethodRequiresImplementations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
